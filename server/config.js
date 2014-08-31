8a74431d0aca622c8af55cd4149a03324c83947b
Paul Jensen <paulbjensen@gmail.com>
2014-08-31 10:32:58 +0100

    First intiial commit of supporting Heroku's deploy to heroku button

--- server/config.js
+++ server/config.js
@@ -1,15 +1,25 @@
 'use strict';
 
 
+// Deconstruct the Redis cloud credentials for use with Node's redis client
+//
+if (process.env.REDISCLOUD_URL) {
+	var redisHost = process.env.REDISCLOUD_URL.split('@')[1].split(':')[0],
+			redisPort = process.env.REDISCLOUD_URL.split('@')[1].split(':')[1],
+			redisPass = process.env.REDISCLOUD_URL.split(':')[2].split('@')[0];
+}
+
+
 
 module.exports = {
 
 	development: {
 		port: 3000,
-		db: 'localhost/dashku_development',
+		db: process.env.MONGOLAB_URI || 'mongodb://localhost/dashku_development',
 		redis: {
-			port: 6379,
-			host: '127.0.0.1'
+			port: redisPort || 6379,
+			host: redisHost || '127.0.0.1',
+			pass: redisPass
 		},
 		apiUrl: 'http://localhost:3000/api/transmission',
 		apiHost: 'http://localhost:3000/',
@@ -31,10 +41,11 @@ module.exports = {
 
 	cucumber: {
 		port: 3001,
-		db: 'localhost/dashku_cucumber',
+		db: 'mongodb://localhost/dashku_cucumber',
 		redis: {
-			port: 6379,
-			host: '127.0.0.1'
+			port: redisPort || 6379,
+			host: redisHost || '127.0.0.1',
+			pass: redisPass
 		},
 		apiUrl: 'http://localhost:3001/api/transmission',
 		apiHost: 'http://localhost:3001/',
@@ -49,10 +60,11 @@ module.exports = {
 
 	test: {
 		port: 3002,
-		db: 'localhost/dashku_test',
+		db: 'mongodb://localhost/dashku_test',
 		redis: {
-			port: 6379,
-			host: '127.0.0.1'
+			port: redisPort || 6379,
+			host: redisHost || '127.0.0.1',
+			pass: redisPass
 		},
 		apiUrl: 'http://localhost:3002/api/transmission',
 		apiHost: 'http://localhost:3002/',
